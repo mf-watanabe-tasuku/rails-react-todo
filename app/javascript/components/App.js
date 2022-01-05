d797ebedc680e8535a0a6332fc93d76dd501cd5f
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2022-01-05 20:30:25 +0900

    add contents to component files

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,1 +1,66 @@
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import styled from 'styled-components'
+import AddTodo from './AddTodo'
+import TodoList from './TodoList'
+import EditTodo from './EditTodo'
+import './App.css'
 
+const NavBar = styled.nav`
+  background: #dbfffe;
+  min-height: 8vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`
+
+const Logo = styled.div`
+  font-weight: bold;
+  font-size: 23px;
+  letter-spacing: 3px;
+`
+
+const NavItems = styled.ul`
+  display: flex;
+  width: 400px;
+  max-width: 40%;
+  justify-content: space-around;
+  list-style: none;
+`
+
+const NavItem = styled.li`
+  font-size: 19px;
+  font-weight: bold;
+  opacity: 0.7;
+  &:hover {
+    opacity: 1;
+  }
+`
+
+const Wrapper = styled.div`
+  width: 700px;
+  max-width: 85%;
+  margin: 20px auto;
+`
+
+function App() {
+  return (
+    <>
+      <NavBar>
+        <Logo>
+          TODO
+        </Logo>
+        <NavItems>
+          <NavItem>
+            <Link to="/todos">Todos</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/todos/new">Add New Todo</Link>
+          </NavItem>
+        </NavItems>
+      </NavBar>
+    </>
+  )
+}
+
+export default App
