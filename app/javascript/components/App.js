347142071bf028105d5101c8fb71eb72483875f5
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2022-01-06 22:58:16 +0900

    add react-router-dom routing

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import styled from 'styled-components'
 import AddTodo from './AddTodo'
 import TodoList from './TodoList'
@@ -59,6 +59,13 @@ function App() {
           </NavItem>
         </NavItems>
       </NavBar>
+      <Wrapper>
+        <Routes>
+          <Route exact path="/todos" element={<TodoList />} />
+          <Route exact path="/todos/new" element={<AddTodo />} />
+          <Route path="/todos/:id/edit" element={<EditTodo />} />
+        </Routes>
+      </Wrapper>
     </>
   )
 }
