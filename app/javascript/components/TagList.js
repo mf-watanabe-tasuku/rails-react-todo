f677d18a2473c9c4ce26c1a64cb57f0d7b47c42e
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2022-01-12 00:59:51 +0900

    add TagList component content

--- app/javascript/components/TagList.js
+++ app/javascript/components/TagList.js
@@ -4,10 +4,55 @@ import axios from 'axios'
 import styled from 'styled-components'
 import { AiFillEdit } from 'react-icons/ai'
 
+const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 7px auto;
+  padding: 10px 0;
+  font-size: 25px;
+`
+
+const TagName = styled.div`
+  font-size: 27px;
+`
+
+const EditButton = styled.span`
+  display: flex;
+  align-items: center;
+  margin: 0 7px;
+`
+
 const TagList = () => {
+  const [tags, setTags] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/v1/tags.json')
+      .then(resp => {
+        setTags(resp.data);
+      })
+      .catch(e => {
+        console.log(e);
+      })
+  }, []);
+
   return (
     <>
-      TagList
+      <h1>TagList</h1>
+      <div>
+        {tags.map((val, key) => {
+          return (
+            <Row key={key}>
+              <TagName>{val.name}</TagName>
+              <Link to={"/tags/" + val.id + "/edit"}>
+                <EditButton>
+                  <AiFillEdit />
+                </EditButton>
+              </Link>
+            </Row>
+          )
+        })}
+      </div>
     </>
   )
 }
